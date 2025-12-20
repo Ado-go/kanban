040a1c9b0ca99273430543c19736066d251f4990
Adogo <gonda.ado@gmail.com>
2025-12-20 15:29:25 +0100

    feat: basic controls + fake data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import { useState } from "react";
+import type { BoardTypes } from "./types/boardTypes";
 import Board from "./components/Board";
+import Button from "./components/Button";
 
 function App() {
-  const [boardData, setBoardData] = useState([]);
+  const [boardData, setBoardData] = useState<BoardTypes>({
+    name: "Work",
+    columns: [
+      {
+        id: 0,
+        title: "Name or status maybe",
+        cards: [
+          {
+            id: 0,
+            title: "task title",
+            description: "What exactly has to be done, or in more details",
+            status: "todo",
+          },
+        ],
+      },
+    ],
+  });
 
   return (
-    <>
+    <div className="m-5">
       <h1>Kanban-ana</h1>
-      <Board name="Work" columns={boardData} />
-    </>
+      <Board name={boardData.name} columns={boardData.columns} />
+      <input type="text" className="border" />
+      <div>
+        <Button text="upload data" handleClick={() => null} />
+        <Button text="export data" handleClick={() => null} />
+      </div>
+    </div>
   );
 }
 
