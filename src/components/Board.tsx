a4619d6786524698addd7727fc040875487ea442
Adogo <gonda.ado@gmail.com>
2025-12-20 20:12:23 +0100

    feat: grid change for columns

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,21 @@
 import type { BoardTypes } from "../types/boardTypes";
 import Column from "./Column";
+import Button from "./Button";
 
 export default function Board({ name, columns }: BoardTypes) {
   return (
     <div className="bg-gray-600 h-150 flex flex-col">
-      <h1 className="p-3 font-bold">{name}</h1>
-      {columns.map((column) => (
-        <Column key={column.id} title={column.title} cardsData={column.cards} />
-      ))}
+      <h1 className="bg-gray-700 p-3 font-bold">{name}</h1>
+      <div className="flex h-full gap-5 p-3">
+        {columns.map((column) => (
+          <Column
+            key={column.id}
+            title={column.title}
+            cardsData={column.cards}
+          />
+        ))}
+        <Button text="+" handleClick={() => null} />
+      </div>
     </div>
   );
 }
