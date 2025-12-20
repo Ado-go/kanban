d3e2ab61412cb39f1900e615a55ec671584aa4d3
Adogo <gonda.ado@gmail.com>
2025-12-20 20:27:05 +0100

    feat: style card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,14 +6,17 @@ type CardProps = {
 
 export default function Card({ title, description, badges }: CardProps) {
   return (
-    <div>
-      <div>
+    <div className="m-2 border-2">
+      <h1 className="text-center bg-gray-700">{title}</h1>
+      <div className="p-2 flex gap-2">
         {badges.map((badge) => (
-          <div key={badge}>{badge}</div>
+          <div className="border-2 rounded pl-2 pr-2" key={badge}>
+            <p className="text-xs">{badge}</p>
+          </div>
         ))}
+        <div className="cursor-pointer select-none">+</div>
       </div>
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <p className="p-2 w-80">{description}</p>
     </div>
   );
 }
