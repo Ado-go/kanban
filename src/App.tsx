e186a6168aa154bc20740b900e4266fcc2514f58
Adogo <gonda.ado@gmail.com>
2025-12-20 18:42:10 +0100

    feat: add data export + import setup

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,37 @@ function App() {
     columns: [
       {
         id: 0,
-        title: "Name or status maybe",
+        title: "Todo",
         cards: [
           {
             id: 0,
             title: "task title",
             description: "What exactly has to be done, or in more details",
-            status: "todo",
+            badges: ["new", "fast"],
           },
         ],
       },
     ],
   });
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div className="m-5">
       <h1>Kanban-ana</h1>
       <Board name={boardData.name} columns={boardData.columns} />
-      <input type="text" className="border" />
+      <input
+        type="text"
+        className="border"
+        placeholder="Insert data"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
       <div>
         <Button text="upload data" handleClick={() => null} />
-        <Button text="export data" handleClick={() => null} />
+        <Button
+          text="export data"
+          handleClick={() => setInputValue(JSON.stringify(boardData))}
+        />
       </div>
     </div>
   );
